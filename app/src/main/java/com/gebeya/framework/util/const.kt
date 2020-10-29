0c87db603f8ddcc3a26fdba666513d2a1c284d8a
Egide Muhire <saladthieves@gmail.com>
2020-10-29 12:55:01 +0200

    Initial commit

---
+++ app/src/main/java/com/gebeya/framework/util/const.kt
@@ -0,0 +1,5 @@
+package com.gebeya.framework.util
+
+const val PREFS_NAME = "misiloch-prefs"
+
+const val DATABASE_NAME = "db-misiloch"
---
+++ app/src/main/java/com/gebeya/misiloch/splash/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.gebeya.misiloch.splash
+
+import android.os.Bundle
+import com.gebeya.framework.base.BaseActivity
+import com.gebeya.misiloch.R
+
+class SplashActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
