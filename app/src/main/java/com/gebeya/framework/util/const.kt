08a929b91eccdde1fc685d9e8091ea496c912718
Egide Muhire <saladthieves@gmail.com>
2020-11-04 11:45:00 +0200

    Add base URL

--- app/src/main/java/com/gebeya/framework/util/const.kt
+++ app/src/main/java/com/gebeya/framework/util/const.kt
@@ -3,3 +3,5 @@ package com.gebeya.framework.util
 const val PREFS_NAME = "misiloch-prefs"
 
 const val DATABASE_NAME = "db-misiloch"
+
+const val BASE_URL = "https://5fa137c42541640016b6ab32.mockapi.io/api/v1/"
